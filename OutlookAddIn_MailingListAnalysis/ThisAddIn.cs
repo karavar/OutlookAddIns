596dd125921224a2cac8466bbd0cd3e41dda1dfd
karavar <karavar@gmail.com>
2014-11-13 20:10:32 -0800

    First Simple Working Solution that updates subject line and content of a new mail.

--- OutlookAddIn_MailingListAnalysis/ThisAddIn.cs
+++ OutlookAddIn_MailingListAnalysis/ThisAddIn.cs
@@ -10,9 +10,30 @@ namespace OutlookAddIn_MailingListAnalysis
 {
     public partial class ThisAddIn
     {
+        Outlook.Inspectors insp;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            insp = this.Application.Inspectors;
+            insp.NewInspector += new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
         }
+
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector insp)
+        {
+            Outlook.MailItem mailItem = insp.CurrentItem as Outlook.MailItem;
+            if(mailItem !=null)
+            {
+                if (mailItem.EntryID == null)
+                {
+                    mailItem.Subject = "I updated this text from code.";
+                    mailItem.Body = "This code works!";
+
+                }
+
+            }
+
+
+        }
+        
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
